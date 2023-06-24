837977fa575f97e426315c6bdb801386637b748d
Bhupi2508 <bhupendrasingh.ec18@gmail.com>
2023-06-25 00:02:18 +0530

    Code refractoring done and node-cron isntalled for notiifcations

--- app/controllers/usersController.js
+++ app/controllers/usersController.js
@@ -26,79 +26,74 @@ import sendmail from '../config/sendMail';
 import { HtmlConstants } from '../constants/htmlContent';
 
 
-/*********************************************************************************
-* Create A User
-* @param {object} req
-* @param {object} res
-* @returns {object} reflection object
-*********************************************************************************/
+const moment = require('moment');
+const { isEmpty, isValidEmail, validatePassword, hashPassword } = require('./utils/validation');
+const { generateUserToken } = require('./utils/token');
+const { sendmail } = require('./utils/email');
+const { Constants, HtmlConstants, status, successMessage, errorMessage } = require('./constants');
+
+/**
+ * Create a User
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} reflection object
+ */
 const createUser = async (req, res) => {
-    const { email, username, first_name, last_name, password } = req.body;
-
-    // timestamp for user creation
-    const created_on = moment(new Date());
-    const update_at = moment(new Date());
-
-    // Parameter Check
-    if (isEmpty(email) || isEmpty(username) || isEmpty(first_name) || isEmpty(last_name) || isEmpty(password)) {
-        errorMessage.error = 'Email, password, first name and last name field cannot be empty';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    // Email Validation
-    if (!isValidEmail(email)) {
-        errorMessage.error = 'Please enter a valid Email';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    // Password alidation
-    if (!validatePassword(password)) {
-        errorMessage.error = 'Password must be more than five(5) characters';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    // Password encryption
-    const hashedPassword = hashPassword(password);
-
-    // db query used to ingest a user with provided values
-    const createUserQuery = Constants.REGISTER_QUERY;
-    const values = [
-        email,
-        username,
-        first_name,
-        last_name,
-        hashedPassword,
-        created_on,
-        update_at
-    ];
-
     try {
+        const { email, username, first_name, last_name, password } = req.body;
+
+        // Parameter Check
+        if (isEmpty(email) || isEmpty(username) || isEmpty(first_name) || isEmpty(last_name) || isEmpty(password)) {
+            errorMessage.error = 'Email, password, first name, and last name field cannot be empty';
+            return res.status(status.bad).send(errorMessage);
+        }
+
+        // Email Validation
+        if (!isValidEmail(email)) {
+            errorMessage.error = 'Please enter a valid Email';
+            return res.status(status.bad).send(errorMessage);
+        }
+
+        // Password Validation
+        if (!validatePassword(password)) {
+            errorMessage.error = 'Password must be more than five(5) characters';
+            return res.status(status.bad).send(errorMessage);
+        }
+
+        // Password Encryption
+        const hashedPassword = hashPassword(password);
+
+        // Timestamp for user creation
+        const created_on = moment().format();
+        const updated_at = moment().format();
+
+        // Database query used to ingest a user with provided values
+        const createUserQuery = Constants.REGISTER_QUERY;
+        const values = [email, username, first_name, last_name, hashedPassword, created_on, updated_at];
+
         const { rows } = await dbQuery.query(createUserQuery, values);
         const dbResponse = rows[0];
         delete dbResponse.password;
 
-        console.log("create user dbResponse ::::: ", dbResponse);
-
-        // A token has been generated for the logged-in user and attached into response
+        // Generate token for the logged-in user
         const token = generateUserToken(dbResponse.email, dbResponse.id, dbResponse.first_name, dbResponse.last_name, dbResponse.username);
         successMessage.data = dbResponse;
 
         // User full name
-        const userFullName = `"${successMessage.data.first_name} ${successMessage.data.last_name}"`;
-        const subject = `Action Required: Verify Your Account for ${userFullName}`
+        const userFullName = `${successMessage.data.first_name} ${successMessage.data.last_name}`;
+        const subject = `Action Required: Verify Your Account for ${userFullName}`;
         const verificationUrl = `http://localhost:3000/v1/verification?token=${encodeURIComponent(token)}`;
-
         const url = HtmlConstants.geterificationContent(verificationUrl);
 
-        // sendmail for the verification
+        // Send verification email
         sendmail.sendmailServices(url, subject, true);
         successMessage.message = "Signup email has been sent successfully! Please check your email and verify your account";
 
         return res.status(status.created).send(successMessage);
     } catch (error) {
-        console.log("error ", error);
+        console.error("Error: ", error);
         if (error.routine === '_bt_check_unique') {
-            errorMessage.error = 'User with that EMAIL already exist';
+            errorMessage.error = 'User with that EMAIL already exists';
             return res.status(status.conflict).send(errorMessage);
         }
         errorMessage.error = 'Operation was not successful';
@@ -106,26 +101,25 @@ const createUser = async (req, res) => {
     }
 };
 
-
-/***********************************************************************************
-* delete
-* @param {object} req
-* @param {object} res
-* @returns {object} user object
-**********************************************************************************/
+/**
+ * Delete a User
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} user object
+ */
 const deleteUser = async (req, res) => {
-    const { email } = req.body;
-
-    // Email check
-    if (!email) {
-        const errorMessage = { error: 'Email is missing' };
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    // db query used to delete the existing user
-    const deleteUserQuery = Constants.DELETE_QUERY;
-
     try {
+        const { email } = req.body;
+
+        // Email check
+        if (!email) {
+            const errorMessage = { error: 'Email is missing' };
+            return res.status(status.bad).send(errorMessage);
+        }
+
+        // Database query used to delete the existing user
+        const deleteUserQuery = Constants.DELETE_QUERY;
+
         const { rowCount } = await dbQuery.query(deleteUserQuery, [email]);
 
         // If email doesn't exist in the table
@@ -137,78 +131,74 @@ const deleteUser = async (req, res) => {
         const successMessage = { message: 'User deleted successfully' };
         return res.status(status.success).send(successMessage);
     } catch (error) {
+        console.error("Error: ", error);
         const errorMessage = { error: 'Operation was not successful' };
         return res.status(status.error).send(errorMessage);
     }
 };
 
-
-/***********************************************************************************
-* fetch all the users
-* @param {object} req
-* @param {object} res
-* @returns {object} user object
-**********************************************************************************/
-const getAllUser = async (req, res) => {
-
-    // db query used to delete the existing user
-    const getAllUserQuery = Constants.GETALLUSER_QUERY;
-
+/**
+ * Fetch all users
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} user object
+ */
+const getAllUsers = async (req, res) => {
     try {
-        const { rows, rowCount } = await dbQuery.query(getAllUserQuery, null);
+        // Database query to fetch all users
+        const getAllUsersQuery = Constants.GETALLUSER_QUERY;
 
-        // If email doesn't exist in the table
+        const { rows, rowCount } = await dbQuery.query(getAllUsersQuery, null);
+
+        // If no records found
         if (rowCount === 0) {
             const errorMessage = { error: 'No Record Found' };
             return res.status(status.notfound).send(errorMessage);
         }
 
-        // response strucutre
-        const finalResponse = [];
+        // Response structure
+        const finalResponse = rows.map(row => ({
+            userName: row.username,
+            email: row.email,
+            requestedDate: row.created_on,
+            verification: row.verification
+        }));
 
-        rows.map(req => {
-            finalResponse.push({
-                userName: req.username,
-                email: req.email,
-                requestedDate: req.created_on,
-                verification: req.verification
-            })
-        })
-
-        const successMessage = { data: [...finalResponse] };
+        const successMessage = { data: finalResponse };
         return res.status(status.success).send(successMessage);
     } catch (error) {
+        console.error("Error: ", error);
         const errorMessage = { error: 'Operation was not successful' };
         return res.status(status.error).send(errorMessage);
     }
 };
 
-
-/***********************************************************************************
-* signin
-* @param {object} req
-* @param {object} res
-* @returns {object} user object
-**********************************************************************************/
-const siginUser = async (req, res) => {
-    const { email, password } = req.body;
-
-    // Email & Password Check
-    if (isEmpty(email) || isEmpty(password)) {
-        errorMessage.error = 'Email or Password detail is missing';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    // Password Validation
-    if (!isValidEmail(email) || !validatePassword(password)) {
-        errorMessage.error = 'Please enter a valid Email or Password';
-        return res.status(status.bad).send(errorMessage);
-    }
-
-    // Find the userin exising table based on req params
-    const signinUserQuery = Constants.LOGIN_QUERY;
+/**
+ * Sign in a User
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} user object
+ */
+const signInUser = async (req, res) => {
     try {
-        const { rows } = await dbQuery.query(signinUserQuery, [email]);
+        const { email, password } = req.body;
+
+        // Email & Password Check
+        if (isEmpty(email) || isEmpty(password)) {
+            errorMessage.error = 'Email or Password detail is missing';
+            return res.status(status.bad).send(errorMessage);
+        }
+
+        // Password Validation
+        if (!isValidEmail(email) || !validatePassword(password)) {
+            errorMessage.error = 'Please enter a valid Email or Password';
+            return res.status(status.bad).send(errorMessage);
+        }
+
+        // Find the user in the existing table based on req params
+        const signInUserQuery = Constants.LOGIN_QUERY;
+
+        const { rows } = await dbQuery.query(signInUserQuery, [email]);
         const dbResponse = rows[0];
 
         // If email doesn't exist
@@ -217,9 +207,8 @@ const siginUser = async (req, res) => {
             return res.status(status.notfound).send(errorMessage);
         }
 
-
         // User is not verified. Please verify user before login
-        if (dbResponse.verification === false) {
+        if (!dbResponse.verification) {
             errorMessage.error = 'User is not verified. Please verify user before login';
             return res.status(status.notfound).send(errorMessage);
         }
@@ -230,37 +219,34 @@ const siginUser = async (req, res) => {
             return res.status(status.bad).send(errorMessage);
         }
 
-        // In next PR will also update these files
         const token = generateUserToken(dbResponse.email, dbResponse.id, dbResponse.first_name, dbResponse.last_name, dbResponse.username);
         delete dbResponse.password;
         successMessage.data = dbResponse;
         successMessage.data.token = token;
+
         return res.status(status.success).send(successMessage);
     } catch (error) {
+        console.error("Error: ", error);
         errorMessage.error = 'Operation was not successful';
         return res.status(status.error).send(errorMessage);
     }
 };
 
-
-/***********************************************************************************
-* verification
-* @param {object} req
-* @param {object} res
-* @returns {object} user object
-**********************************************************************************/
+/**
+ * User Verification
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} user object
+ */
 const verification = async (req, res) => {
-    const { email, user_id } = req.user;
-
-    // Find the userin exising table based on req params
-    const signinUserQuery = Constants.FETCHDATA_QUERY;
     try {
+        const { email, user_id } = req.user;
 
-        const { rows } = await dbQuery.query(signinUserQuery, [email, user_id]);
+        // Find the user in the existing table based on req params
+        const fetchUserQuery = Constants.FETCHDATA_QUERY;
+
+        const { rows } = await dbQuery.query(fetchUserQuery, [email, user_id]);
         const dbResponse = rows[0];
-        delete dbResponse.password;
-
-        console.log("dbResponse ::::::: ", dbResponse)
 
         // If email doesn't exist
         if (!dbResponse) {
@@ -268,7 +254,7 @@ const verification = async (req, res) => {
             return res.status(status.notfound).send(errorMessage);
         }
 
-        successMessage.message = `User verification successful. You can now proceed to login`;
+        successMessage.message = 'User verification successful. You can now proceed to login';
         successMessage.data = {
             email: dbResponse.email,
             verification: dbResponse.verification
@@ -276,28 +262,27 @@ const verification = async (req, res) => {
 
         return res.status(status.success).send(successMessage);
     } catch (error) {
+        console.error("Error: ", error);
         errorMessage.error = 'Error in user verification! Please try again';
         return res.status(status.error).send(errorMessage);
     }
 };
 
-
-/***********************************************************************************
-* forgotPassword
-* @param {object} req
-* @param {object} res
-* @returns {object} user object
-**********************************************************************************/
+/**
+ * Forgot Password
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} user object
+ */
 const forgotPassword = async (req, res) => {
-    const { email } = req.body;
-
-    // Find the userin exising table based on req params
-    const signinUserQuery = Constants.LOGIN_QUERY;
     try {
+        const { email } = req.body;
 
-        const { rows } = await dbQuery.query(signinUserQuery, [email]);
+        // Find the user in the existing table based on req params
+        const findUserQuery = Constants.LOGIN_QUERY;
+
+        const { rows } = await dbQuery.query(findUserQuery, [email]);
         const dbResponse = rows[0];
-        console.log("dbResponse ::::::: ", dbResponse)
 
         // If email doesn't exist
         if (!dbResponse) {
@@ -305,11 +290,10 @@ const forgotPassword = async (req, res) => {
             return res.status(status.notfound).send(errorMessage);
         }
 
-
-        // A token has been generated for the reset password
+        // Generate a token for password reset
         const token = generateUserToken(dbResponse.email, dbResponse.id, dbResponse.first_name, dbResponse.last_name, dbResponse.username);
 
-        successMessage.message = `we've sent password reset instructions to the primary email address on the account`;
+        successMessage.message = 'We have sent password reset instructions to the primary email address on the account';
         successMessage.data = {
             email: dbResponse.email,
             username: dbResponse.username,
@@ -319,32 +303,33 @@ const forgotPassword = async (req, res) => {
         const verificationUrl = `http://localhost:3000/v1/resetPasswordHtmlPage?token=${encodeURIComponent(token)}`;
         const tokenData = HtmlConstants.getTokenContent(verificationUrl);
 
-        const subject = `Password Reset Request for :: ${dbResponse.first_name} ${dbResponse.last_name}`;
+        const subject = `Password Reset Request for ${dbResponse.first_name} ${dbResponse.last_name}`;
 
-        // sendmail for the verification
+        // Send email for verification
         sendmail.sendmailServices(tokenData, subject, true);
 
         return res.status(status.success).send(successMessage);
     } catch (error) {
+        console.error("Error: ", error);
         errorMessage.error = 'Error during forgot password! Please try again';
         return res.status(status.error).send(errorMessage);
     }
 };
 
-
-/***********************************************************************************
-* resetPassword
-* @param {object} req
-* @param {object} res
-* @returns {object} user object
-**********************************************************************************/
+/**
+ * Reset Password
+ * @param {object} req
+ * @param {object} res
+ * @returns {object} user object
+ */
 const resetPassword = async (req, res) => {
-    const { email } = req.user;
-    const { password, confirmPassword } = req.body;
-
-    // Find the user in the existing table based on req params
-    const resetPasswordQuery = Constants.RESETPASS_QUERY;
     try {
+        const { email } = req.user;
+        const { password, confirmPassword } = req.body;
+
+        // Find the user in the existing table based on req params
+        const resetPasswordQuery = Constants.RESETPASS_QUERY;
+
         // Password and ConfirmPassword should be the same
         if (password !== confirmPassword) {
             errorMessage.error = 'Password and confirm password should be the same';
@@ -362,7 +347,6 @@ const resetPassword = async (req, res) => {
 
         const { rows } = await dbQuery.query(resetPasswordQuery, [hashedPassword, email]);
         const dbResponse = rows[0];
-        console.log("dbResponse ::::::: ", dbResponse)
 
         // If email doesn't exist
         if (!dbResponse) {
@@ -378,38 +362,37 @@ const resetPassword = async (req, res) => {
 
         return res.status(status.success).send(successMessage);
     } catch (error) {
+        console.error("Error: ", error);
         errorMessage.error = 'Error in resetting password! Please try again.';
         return res.status(status.error).send(errorMessage);
     }
 };
 
-
-/***********************************************************************************
-* resetPassword
+/**
+* Reset Password from HTML Page
 * @param {object} req
 * @param {object} res
 * @returns {object} user object
-**********************************************************************************/
+*/
 const resetPasswordFromHTMLPage = async (req, res) => {
-    const paramToken = req.url.split("token=")[1];
+    const paramToken = req.query.token;
+
     try {
-        
         const tokenData = HtmlConstants.resetPasswordSubmitPage(paramToken);
         return res.send(tokenData);
     } catch (error) {
-        console.log(error);
+        console.error("Error: ", error);
         errorMessage.error = 'Error! Please try again.';
         return res.status(status.error).send(errorMessage);
     }
 };
 
 
-
 export {
     createUser,
-    siginUser,
+    signInUser,
     deleteUser,
-    getAllUser,
+    getAllUsers,
     verification,
     forgotPassword,
     resetPassword,
